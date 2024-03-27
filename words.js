95c0e7a6aeecc11a4bc7838826c1b53e3d33b4d2
bipashabg <bipasha_bg@es.iitr.ac.in>
2024-03-27 16:44:39 +0530

    add source files and background image

---
+++ words.js
@@ -0,0 +1,62 @@
+let words = [
+    {
+        word: "libra",
+        hint: "The only zodiac sign constellation that does not symbolize a living creature."
+    },
+    {
+        word: "cygnus",
+        hint: "This constellation is home not only to the asterism called the Northern Cross but also to a binary star that is the first object widely held to be a black hole."
+    },
+    {
+        word: "sirius",
+        hint: "Canis Major contains this star, the brightest in the night sky."
+    },
+    {
+        word: "plough",
+        hint: "Ursa Major which served as a pointer to north direction for millennia has this well-recognized asterism that can also be found on Alaska's flag."
+    },
+    {
+        word: "ptolemy",
+        hint: "The current set of constellations recognized by the International Astronomical Union has its basis in a list in Almagest written in the 2nd century by this Greek."
+    },
+    {
+        word: "mensa",
+        hint: "The only constellation named for a man-made object is this, also the name of a society in which members are said to have an high IQ."
+    },
+    {
+        word: "aquarius",
+        hint: "One of the constellations in the zodiac, its name is Latin for 'water carrier'. It is also the namesake of an 'age' which will dawn in 2597 AD."
+    },
+    {
+        word: "betelgeuse",
+        hint: "The brightest star in Orion"
+    },
+    {
+        word: "andromeda",
+        hint: "Nearest galaxy to the Milky Way"
+    },
+    {
+        word: "nebula",
+        hint: "A star begins its life in a cloud of gas and dust."
+    },
+    {
+        word: "protostars",
+        hint: "Glowing areas in the birth place of a star."
+    },
+    {
+        word: "pegasus",
+        hint: "This constellation is often depicted as a winged horse and contains a square-shaped asterism."
+    },
+    {
+        word: "lyra",
+        hint: "This constellation is named after a musical instrument."
+    },
+    {
+        word: "hyades",
+        hint: "Open star cluster located in the constellation Taurus"
+    },
+    {
+        word: "quasar",
+        hint: " Compact region in the center of a massive galaxy that surrounds a supermassive black hole"
+    }
+]
